a3400e89231d72a271fd1c1c8ddfca439667aa5a
Jordi Orlando <jordi.orlando@gmail.com>
2016-10-10 13:20:44 -0500

    Large changes - Refactor code - Require external rather than inline drill JSON - Only select one performer at a time - Parse and display dot locations - Support sequential moves - Add metronome

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const HEIGHT  = 160;
 
 const LINE    = 1/3;
 const MARKER  = 3;
-const MARCHER = 3;
+const MARCHER = 3.5;
 const NUMBERS = {
   top: 27,
   size: 6
@@ -28,421 +28,21 @@ const PRO = {
 const COLORS = {
   field:    '#689f38',
   endzone:  '#e04e39',
-  marcher:  {
-    icon: '#e04e39',
-    text: '#13294b'
+  performer:  {
+    fill:   '#e04e39',
+    stroke: '#13294b',
+    text:   '#fff'
   }
 };
 
 
 
-var sets = [
-  {
-    name: '1',
-    counts: 0,
-    tempo: 120
-  },
-  {
-    name: '2',
-    counts: 16,
-    tempo: 120
-  },
-  {
-    name: '3',
-    counts: 16,
-    tempo: 120
-  },
-  {
-    name: '4',
-    counts: 16,
-    tempo: 120
-  },
-  {
-    name: '5',
-    counts: 16,
-    tempo: 120
-  },
-  {
-    name: '6',
-    counts: 16,
-    tempo: 120
-  },
-  {
-    name: '7',
-    counts: 16,
-    tempo: 120
-  },
-  {
-    name: '8',
-    counts: 4,
-    tempo: 120
-  }
-];
-var marchers = {
-  'A1': {
-    dots: [
-      {
-        x: -24,
-        y: 64.5
-      },
-      {
-        x: -16,
-        y: 48
-      },
-      {
-        x: -32,
-        y: 48
-      },
-      {
-        x: -44,
-        y: 44
-      },
-      {
-        x: -60,
-        y: 34
-      },
-      {
-        x: -60,
-        y: 34
-      },
-      {
-        x: -64,
-        y: 17
-      },
-      {
-        x: -76,
-        y: 28
-      }
-    ]
-  },
-  'A2': {
-    dots: [
-      {
-        x: -6,
-        y: 64
-      },
-      {
-        x: -12,
-        y: 48
-      },
-      {
-        x: -28,
-        y: 48
-      },
-      {
-        x: -44,
-        y: 48
-      },
-      {
-        x: -58,
-        y: 35.25
-      },
-      {
-        x: -58,
-        y: 35.25
-      },
-      {
-        x: -63.75,
-        y: 19.5
-      },
-      {
-        x: -72,
-        y: 28
-      }
-    ]
-  },
-  'A3': {
-    dots: [
-      {
-        x: -6,
-        y: 56
-      },
-      {
-        x: -12,
-        y: 40
-      },
-      {
-        x: -28,
-        y: 40
-      },
-      {
-        x: -40,
-        y: 44
-      },
-      {
-        x: -55.75,
-        y: 36.5
-      },
-      {
-        x: -55.75,
-        y: 36.5
-      },
-      {
-        x: -63,
-        y: 21.75
-      },
-      {
-        x: -68,
-        y: 28
-      }
-    ]
-  },
-  'A4': {
-    dots: [
-      {
-        x: -4,
-        y: 64
-      },
-      {
-        x: -8,
-        y: 48
-      },
-      {
-        x: -24,
-        y: 48
-      },
-      {
-        x: -40,
-        y: 48
-      },
-      {
-        x: -53.5,
-        y: 37.75
-      },
-      {
-        x: -53.5,
-        y: 37.75
-      },
-      {
-        x: -62,
-        y: 24.25
-      },
-      {
-        x: -64,
-        y: 28
-      }
-    ]
-  },
-  'A5': {
-    dots: [
-      {
-        x: -8,
-        y: 56
-      },
-      {
-        x: -16,
-        y: 40
-      },
-      {
-        x: -32,
-        y: 40
-      },
-      {
-        x: -36,
-        y: 44
-      },
-      {
-        x: -51.25,
-        y: 39
-      },
-      {
-        x: -51.25,
-        y: 39
-      },
-      {
-        x: -60.75,
-        y: 26.5
-      },
-      {
-        x: -60,
-        y: 28
-      }
-    ]
-  },
-  'A6': {
-    dots: [
-      {
-        x: -2,
-        y: 64
-      },
-      {
-        x: -4,
-        y: 48
-      },
-      {
-        x: -20,
-        y: 48
-      },
-      {
-        x: -36,
-        y: 48
-      },
-      {
-        x: -48.75,
-        y: 40
-      },
-      {
-        x: -48.75,
-        y: 40
-      },
-      {
-        x: -58.75,
-        y: 28.25
-      },
-      {
-        x: -56,
-        y: 28
-      }
-    ]
-  },
-  'A7': {
-    dots: [
-      {
-        x: -10,
-        y: 56
-      },
-      {
-        x: -20,
-        y: 40
-      },
-      {
-        x: -36,
-        y: 40
-      },
-      {
-        x: -32,
-        y: 44
-      },
-      {
-        x: -46.5,
-        y: 40.75
-      },
-      {
-        x: -46.5,
-        y: 40.75
-      },
-      {
-        x: -56.75,
-        y: 29.75
-      },
-      {
-        x: -52,
-        y: 28
-      }
-    ]
-  },
-  'A8': {
-    dots: [
-      {
-        x: 0,
-        y: 64
-      },
-      {
-        x: 0,
-        y: 48
-      },
-      {
-        x: -16,
-        y: 48
-      },
-      {
-        x: -32,
-        y: 48
-      },
-      {
-        x: -44,
-        y: 41.5
-      },
-      {
-        x: -44,
-        y: 41.5
-      },
-      {
-        x: -54.5,
-        y: 30.75
-      },
-      {
-        x: -48,
-        y: 28
-      }
-    ]
-  },
-  'A9': {
-    dots: [
-      {
-        x: -24,
-        y: 54.5
-      },
-      {
-        x: -24,
-        y: 40
-      },
-      {
-        x: -40,
-        y: 40
-      },
-      {
-        x: -28,
-        y: 44
-      },
-      {
-        x: -41.5,
-        y: 42.25
-      },
-      {
-        x: -41.5,
-        y: 42.25
-      },
-      {
-        x: -52,
-        y: 31.5
-      },
-      {
-        x: -44,
-        y: 28
-      }
-    ]
-  },
-  'A10': {
-    dots: [
-      {
-        x: 2,
-        y: 64
-      },
-      {
-        x: 4,
-        y: 48
-      },
-      {
-        x: -12,
-        y: 48
-      },
-      {
-        x: -28,
-        y: 48
-      },
-      {
-        x: -39,
-        y: 42.75
-      },
-      {
-        x: -39,
-        y: 42.75
-      },
-      {
-        x: -49.5,
-        y: 32
-      },
-      {
-        x: -40,
-        y: 28
-      }
-    ]
-  }
-};
+// Global variables
+var drill = {};
 var hashes = [
   COLLEGE
 ];
+var selected;
 
 
 
@@ -490,94 +90,96 @@ SVG.on(window, 'resize', function () { draw.spof(); });
 
 
 
-var drawYardLine = function(yard) {
-  yard *= 3;
-  let line = draw.group();
+// Draw field and markings
+var drawField = function() {
+  var drawYardLine = function(yard) {
+    yard *= 3;
+    let line = draw.group();
 
-  // Yard line
-  line.line(yard, 0, yard, -HEIGHT)
-      .stroke({
-        color: '#fff',
-        width: LINE
-      });
+    // Yard line
+    line.line(yard, 0, yard, -HEIGHT)
+        .stroke({
+          color: '#fff',
+          width: LINE
+        });
 
-  let drawMarker = function(vert) {
-    return line.line(yard - MARKER/2, vert, yard + MARKER/2, vert)
-      .stroke({
-        color: '#fff',
-        width: LINE
-      });
-  };
-  let drawYardMarkers = function(vert) {
-    if (yard != 0) {
-      for (let i = 3; i < 15; i += 3) {
-        drawMarker(vert - MARKER/2)
-          .translate(yard < 0 ? i : -i, 0)
-          .rotate(90);
+    let drawMarker = function(vert) {
+      return line.line(yard - MARKER/2, vert, yard + MARKER/2, vert)
+        .stroke({
+          color: '#fff',
+          width: LINE
+        });
+    };
+    let drawYardMarkers = function(vert) {
+      if (yard != 0) {
+        for (let i = 3; i < 15; i += 3) {
+          drawMarker(vert - MARKER/2)
+            .translate(yard < 0 ? i : -i, 0)
+            .rotate(90);
+        }
+      }
+    };
+    let drawNumbers = function() {
+      let text = line.text(String(50 - Math.abs(yard / 3)))
+        .font({
+          family: 'Helvetica',
+          size:   NUMBERS.size,
+          anchor: 'middle'
+        })
+        .fill({
+          color: '#fff'
+        });
+      // text.on('mousedown', e => e.preventDefault());
+      return text;
+    };
+
+    drawYardMarkers(0);
+    drawYardMarkers(-(HEIGHT - MARKER));
+
+    for (let hash of hashes) {
+      drawYardMarkers(-(hash.front - MARKER));
+      drawYardMarkers(-(hash.front + hash.between));
+
+      if (Math.abs(yard) != 150) {
+        // Front hash
+        line.line(yard - MARKER/2, -hash.front, yard + MARKER/2, -hash.front)
+            .stroke({
+              color: '#fff',
+              width: LINE
+            });
+
+        // Back hash
+        line.line(yard - MARKER/2, -(hash.front + hash.between), yard + MARKER/2, -(hash.front + hash.between))
+            .stroke({
+              color: '#fff',
+              width: LINE
+            });
       }
     }
-  };
-  let drawNumbers = function() {
-    let text = line.text(String(50 - Math.abs(yard / 3)))
-      .font({
-        family: 'Helvetica',
-        size:   NUMBERS.size,
-        anchor: 'middle'
-      })
-      .fill({
-        color: '#fff'
-      });
-    // text.on('mousedown', e => e.preventDefault());
-    return text;
-  };
 
-  drawYardMarkers(0);
-  drawYardMarkers(-(HEIGHT - MARKER));
+    switch (Math.abs(yard)) {
+      case 45:
+        drawMarker(-HEIGHT/2).rotate(-45);
+        drawMarker(-HEIGHT/2).rotate(45);
+        break;
+      case 150:
 
-  for (let hash of hashes) {
-    drawYardMarkers(-(hash.front - MARKER));
-    drawYardMarkers(-(hash.front + hash.between));
-
-    if (Math.abs(yard) != 150) {
-      // Front hash
-      line.line(yard - MARKER/2, -hash.front, yard + MARKER/2, -hash.front)
-          .stroke({
-            color: '#fff',
-            width: LINE
-          });
-
-      // Back hash
-      line.line(yard - MARKER/2, -(hash.front + hash.between), yard + MARKER/2, -(hash.front + hash.between))
-          .stroke({
-            color: '#fff',
-            width: LINE
-          });
     }
-  }
 
-  switch (Math.abs(yard)) {
-    case 45:
-      drawMarker(-HEIGHT/2).rotate(-45);
-      drawMarker(-HEIGHT/2).rotate(45);
-      break;
-    case 150:
+    if (!(yard % 30) && Math.abs(yard) != 150) {
+      drawNumbers().move(yard, -NUMBERS.top);
+      drawNumbers().move(yard, -(HEIGHT + NUMBERS.size - NUMBERS.top)).rotate(180);
+    }
+  };
+  var drawEndZone = function(side) {
+    return draw.rect(30, HEIGHT)
+        .translate(165 * side - 15, -HEIGHT)
+        .fill({
+          color: COLORS.endzone,
+          opacity: 1
+        });
+  };
 
-  }
-
-  if (!(yard % 30) && Math.abs(yard) != 150) {
-    drawNumbers().move(yard, -NUMBERS.top);
-    drawNumbers().move(yard, -(HEIGHT + NUMBERS.size - NUMBERS.top)).rotate(180);
-  }
-};
-var drawEndZone = function(side) {
-  return draw.rect(30, HEIGHT)
-      .translate(165 * side - 15, -HEIGHT)
-      .fill({
-        color: COLORS.endzone,
-        opacity: 1
-      });
-};
-var drawField = function() {
   // Field background
   draw.rect(WIDTH + 5, HEIGHT + 5)
       .translate(-(WIDTH/2 + 2.5), -(HEIGHT + 2.5))
@@ -640,35 +242,118 @@ var drawField = function() {
     }
   }
 };
+// Draw all performers
+var drawPerformers = function() {
+  for (let p of drill.performers) {
+    p.selected = false;
+
+    p.icon = draw.group();
+    p.icon.style('cursor', 'pointer');
+    p.icon.circle(MARCHER)
+      .center(0, 0)
+      .fill({
+        color: COLORS.performer.fill,
+        opacity: 1
+      })
+      .stroke({
+        color: COLORS.performer.stroke,
+        width: 0
+      });
+    p.icon.text(_getName(p))
+      .translate(0, -MARCHER/2)
+      .font({
+        family: 'Helvetica',
+        size: MARCHER/2,
+        anchor: 'middle'
+      })
+      .fill({
+        color: COLORS.performer.text
+      });
+
+    p.icon.on('mousedown', function(e) {
+      // e.preventDefault();
+      _selectPerformer(p);
+    });
+  }
+
+  // Load the first set
+  loadSet(1);
+};
 
 var set = 1;
 var playing = false;
 var updateSet = function(dir) {
-  let s = sets[set - 1];
+  let s = drill.sets[set - 1];
   let t = s.counts * 1000 * 60 / s.tempo;
+  if (s.pulse === 'half') {
+    t *= 2;
+  }
 
   let status = `Set ${s.name}`;
   // TODO: fix button positioning (set > 1)
   if (set >= 1) {
-    status += ` (${s.counts} counts at ${s.tempo}bpm)`;
+    status += ` (${s.counts} counts at ${s.tempo}bpm${s.pulse ? ' ' + s.pulse + ' time' : ''})`;
   }
-  document.getElementById('status').firstChild.textContent = status;
+  document.getElementById('status').children[0].firstChild.textContent = status;
+  if (selected) {
+    document.getElementById('status').children[1].children[0].textContent = `Performer ${_getName(selected)}\n${_getHoriz(selected, set - 1)}\n${_getVert(selected, set - 1)}`;
+  }
 
-  for (let m in marchers) {
-    let marcher = marchers[m];
-    let icon = marcher.icon;
+  for (let p of drill.performers) {
+    let icon = p.icon;
+    let count = 0;
+    let metCount;
 
     if (!playing) {
       icon.finish();
     }
-    if (playing && dir == 1) {
-      icon = icon.animate(t);
-    }
 
-    icon.move(
-      marcher.dots[set - 1].x * 15 / 8,
-      -marcher.dots[set - 1].y * 15 / 8
-    );
+    let duringCallback = function(counts) {
+      return function(pos, morph, eased, situation) {
+        pos = parseInt(pos * counts);
+        if (pos !== metCount) {
+          metCount = pos;
+          if (pos < counts) {
+            count++;
+            console.log(count);
+          }
+
+          // document.getElementById(metCount ? 'click2' : 'click1').play();
+        }
+      };
+    };
+
+    if (p.sets[set - 1] instanceof Array) {
+      for (let m of p.sets[set - 1]) {
+        if (playing && dir == 1) {
+          icon = icon.animate(m.counts * 1000 * 60 / s.tempo, '-');
+
+          if (p === drill.performers[0]) {
+            icon = icon.during(duringCallback(m.counts));
+          }
+        }
+
+        if (m.type === "move") {
+          icon = icon.move(
+            m.x * 15 / 8,
+            -m.y * 15 / 8
+          );
+        }
+      }
+    } else {
+      if (playing && dir == 1) {
+        icon = icon.animate(t, '-');
+
+        if (p === drill.performers[0]) {
+          icon = icon.during(duringCallback(s.counts));
+        }
+      }
+
+      icon.move(
+        p.sets[set - 1].x * 15 / 8,
+        -p.sets[set - 1].y * 15 / 8
+      );
+    }
   }
 
   return set;
@@ -686,38 +371,150 @@ var nextSet = function(stop) {
   if (stop) {
     playing = false;
   }
-  if (set < sets.length) {
+  if (set < drill.sets.length) {
     set++;
     return updateSet(1);
-  } else if (set == sets.length) {
+  } else if (set == drill.sets.length) {
     return updateSet(0);
   }
 };
 var loadSet = function(s) {
-  if (s > 0 && s <= sets.length) {
+  if (s > 0 && s <= drill.sets.length) {
     set = s;
     return updateSet(0);
   }
 };
 var playPause = function() {
-  for (let m in marchers) {
+  for (let p of drill.performers) {
     if (playing) {
-      marchers[m].icon.pause();
+      p.icon.pause();
     } else {
-      marchers[m].icon.play();
+      p.icon.play();
     }
   }
 
   playing = !playing;
-  document.getElementById('status').children[2].innerText = playing ? 'Pause' : 'Play';
+  document.getElementById('status').children[0].children[2].innerText = playing ? 'Pause' : 'Play';
 };
 var play = function() {
   playing = false;
   loadSet(set);
   playing = true;
-  while (set != sets.length) {
+
+  // TODO: fix this
+  while (set != drill.sets.length) {
     nextSet();
   }
+};
+var _selectPerformer = function(p) {
+  if (selected !== p) {
+    _selectPerformer(selected);
+    selected = p;
+  } else {
+    selected = undefined;
+  }
+
+  if (p) {
+    p.selected = !p.selected;
+    p.icon.first().stroke({
+      width: p.selected ? 0.25 : 0
+    });
+    document.getElementById('status').children[1].children[0].textContent = p.selected ? `Performer ${_getName(p)}\n${_getHoriz(p, set - 1)}\n${_getVert(p, set - 1)}` : '\n\n\n';
+  }
+};
+var _getName = function(p) {
+  return p.squad !== undefined ? p.squad + p.position : p.type + p.num;
+};
+var _getHoriz = function(p, s) {
+  let x;
+  if (p.sets[s] instanceof Array) {
+    for (let a of p.sets[s]) {
+      if (a.x !== undefined) {
+        x = a.x;
+      }
+    }
+
+    if (x === undefined) {
+      return _getHoriz(p, s - 1);
+    }
+  } else {
+    x = p.sets[s].x;
+  }
+
+  let horiz = x ? (x < 0 ? 'Left: ' : 'Right: ') : '';
+
+  let yard = 80 - Math.abs(x);
+  let offset = yard % 8;
+  offset = offset <= 4 ? offset : offset - 8;
+  yard = (yard - offset) / 8 * 5;
+
+  if (offset) {
+    if (offset === 4) {
+      // horiz += `Splitting ${yard} and ${yard + 5} yd lns`;
+    } else {}
+    horiz += `${Math.abs(offset)} steps ${offset > 0 ? 'inside' : 'outside'} ${yard} yd ln`;
+  } else {
+    horiz += yard ? `On ${yard} yd ln` : 'On goal ln';
+  }
+
+  return p.sets[s].horiz || horiz;
+};
+var _getVert = function(p, s) {
+  let y;
+  if (p.sets[s] instanceof Array) {
+    for (let a of p.sets[s]) {
+      if (a.y !== undefined) {
+        y = a.y;
+      }
+    }
+
+    if (y === undefined) {
+      return _getVert(p, s - 1);
+    }
+  } else {
+    y = p.sets[s].y;
+  }
+
+  let vert;
+  if (y === 0) {
+    vert = 'On Home side line';
+  } else if (y <= 16) {
+    vert = `${y} steps behind Home side line`;
+  } else if (y < 32) {
+    vert = `${32 - y} steps in front of Home hash`;
+  } else if (y === 32) {
+    vert = 'On Home hash'
+  } else if (y <= 42+2/3) {
+    vert = `${y - 32} steps behind Home hash`;
+  } else if (y < 53+1/3) {
+    vert = `${53+1/3 - y} steps in front of Visitor hash`;
+  } else if (y === 53+1/3) {
+    vert = 'On Visitor hash'
+  } else if (y < 69+1/3) {
+    vert = `${y - 53+1/3} steps behind Visitor hash`;
+  } else if (y < 85+1/3) {
+    vert = `${85+1/3 - y} steps in front of Visitor side line`;
+  } else {
+    vert = 'On Visitor side line';
+  }
+
+  return p.sets[s].vert || vert;
+};
+var select = function(name) {
+  for (let p of drill.performers) {
+    if (_getName(p).toUpperCase() === name.toUpperCase()) {
+      _selectPerformer(p);
+      return p;
+    }
+  }
+};
+var loadDrill = function(name) {
+  $.getJSON(`drill/${name}.json`, function(data) {
+    drill = data;
+    document.getElementById('status').children[0].lastChild.textContent = drill.name;
+    drawPerformers();
+    // select(_getName(drill.performers[0]));
+  });
 };
 
 document.addEventListener('keydown', function(e) {
@@ -734,53 +531,8 @@ document.addEventListener('keydown', function(e) {
   }
 }, false);
 
-// Render the field and marchers
-var render = function() {
-  // Draw the field
-  drawField();
-
-  // Draw all marchers
-  for (let m in marchers) {
-    let marcher = marchers[m];
-    marcher.selected = false;
-
-    marcher.icon = draw.group();
-    marcher.icon.style('cursor', 'pointer');
-    marcher.icon.circle(MARCHER)
-      .center(0, 0)
-      .fill({
-        color: COLORS.marcher.icon,
-        opacity: 1
-      })
-      .stroke({
-        color: COLORS.marcher.text,
-        width: 0
-      });
-    marcher.icon.text(m)
-      .translate(0, -MARCHER*2/3)
-      .font({
-        family: 'Helvetica',
-        size: MARCHER*2/3,
-        anchor: 'middle'
-      })
-      .fill({
-        color: COLORS.marcher.text
-      });
-
-    marcher.icon.on('mousedown', function(e) {
-      // e.preventDefault();
-
-      marcher.selected = !marcher.selected;
-      marcher.icon.first().stroke({
-        width: marcher.selected ? 0.25 : 0
-      });
-    });
-  }
-
-  // Load the first set
-  loadSet(1);
-};
 
 
-
-render();
+// Draw the field
+drawField();
+loadDrill('');
